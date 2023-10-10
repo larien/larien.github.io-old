71fb83ad22a4b40890becea513da8e203f918586
Lauren Ferreira <lauren.ferremch@gmail.com>
2023-10-09 21:09:27 -0300

    fix: trigger

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@ html {
 	flex-direction: column;
 }
 
-/* http://meyerweb.com/eric/tools/css/reset/ 
+/*  http://meyerweb.com/eric/tools/css/reset/ 
    v2.0 | 20110126
    License: none (public domain)
 */
